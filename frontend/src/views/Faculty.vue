1e4e89e02c0314eb623df0ec45db82b0450a32aa
Kashaf Parkar <kashaf786parkar@gmail.com>
2025-06-20 14:13:52 +0530

    Faculty extra button removed

--- frontend/src/views/Faculty.vue
+++ frontend/src/views/Faculty.vue
@@ -15,20 +15,9 @@
     <!-- Add Applicants Button -->
     <button
       @click="showApplicantForm = !showApplicantForm"
-      class="bg-pink-600 text-white px-4 py-2 rounded hover:bg-pink-700 mb-6 ml-4"
-    >
-      {{ showApplicantForm ? 'Close' : 'Add Applicants' }}
-    </button>
-
-    <!-- Show Create Exam Form -->
-    <CreateExamForm v-if="showForm" @closeForm="onFormClose" />
-
-    <button
-      @click="showForm = true"
-      v-if="!showForm"
       class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
     >
-      Add Applicant
+      {{ showApplicantForm ? 'Close' : 'Add Applicants' }}
     </button>
 
     <!-- Show Create Exam Form -->
