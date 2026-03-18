a4e1d434fed75f9d7f396eec3eaca2659f53a8bb
Kashaf Parkar <kashaf786parkar@gmail.com>
2026-03-18 11:36:33 +0530

    Back to Localhost

--- backend/db_config.py
+++ backend/db_config.py
@@ -2,10 +2,10 @@ from flask_mysqldb import MySQL
 
 
 def init_mysql(app):
-    app.config['MYSQL_HOST'] = '69.49.230.69:3306'
-    app.config['MYSQL_USER'] = 'kiy_nuv_entrance'
-    app.config['MYSQL_PASSWORD'] = 'bEWeNviz4mn*g^65'
-    app.config['MYSQL_DB'] = 'admin_nuv_entrance'
+    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] = 'Diamond@7115'
+    app.config['MYSQL_DB'] = 'entrance_database'
     
     mysql = MySQL(app)
     return mysql
